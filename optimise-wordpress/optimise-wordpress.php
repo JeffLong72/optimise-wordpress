372f0da7615151270331b7d7b9527185288e4237
Jeff Long <45916412+JeffLong72@users.noreply.github.com>
2019-08-19 11:07:21 +0100

    Update optimise-wordpress.php

--- optimise-wordpress/optimise-wordpress.php
+++ optimise-wordpress/optimise-wordpress.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: . Optimise Wordpress
-Plugin URI:
+Plugin URI: 
 Description: Modifies Wordpress to improve site performance. 1) Moves all .js files to web page footer to prevent render blocking. 2) Disables all Emojis. 3) Removes Wordpress version from source code.
 Version: 1.0.0
 Author: Jeff L
